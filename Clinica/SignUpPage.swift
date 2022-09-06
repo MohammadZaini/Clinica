9bc80acd51ef1e75f61707221fd08c9359f1fc08
abdalrahman301 <khlaileh2000@gmail.com>
2022-09-07 01:57:22 +0300

    //

--- Clinica/SignUpPage.swift
+++ Clinica/SignUpPage.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class SignUpPage: UIViewController {
 
@@ -23,12 +25,75 @@ class SignUpPage: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        validateFields()
         setUpElements()
     }
     
 
     @IBAction func SignUpTapped(_ sender: UIButton) {
+        // Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            // There's something wrong with the fields, show error message
+            showError(error!)
+        }
+        else {
+            
+            // Create cleaned versions of the data
+            let firstName = firstNameTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let phoneNumber = PhoneNumberTextfield.text!.trimmingCharacters(in: .whitespaces)
+            let password = passwordTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                
+                // Check for errors
+                if err != nil {
+                    
+                    // There was an error creating the user
+                    self.showError("Error creating user")
+                }
+                else {
+                    
+                    // User was created successfully, now store the first name and last name
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument(data: ["firstname":firstName, "lastname":lastName,"email":email,"PhoneNumber":phoneNumber,
+                                                              "uid": result!.user.uid ]) { (error) in
+                        
+                        if error != nil {
+                            // Show error message
+                            self.showError("Error saving user data")
+                        }
+                    }
+                    
+                    // Transition to the home screen
+                    self.transitionToHome()
+                }
+                
+            }
+            
+            
+            
+        }
+    }
+    
+    func showError(_ message:String) {
+        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func transitionToHome() {
+        
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeTabBar
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
     }
    
     
@@ -39,5 +104,31 @@ class SignUpPage: UIViewController {
        
         
     }
+    
+    
+    func validateFields() -> String? {
+        
+        // Check that all fields are filled in
+        if firstNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            PhoneNumberTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        // Check if the password is secure
+        let cleanedPassword = passwordTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Please make sure your password is at least 8 characters, contains a special character and a number."
+        }
+        
+        return nil
+    }
+    
+
 
 }
