7a61860cf864f39513bcc5b92fb663715a6c77bc
abdalrahman301 <khlaileh2000@gmail.com>
2022-09-24 22:53:30 +0300

    HealthCare page is done

--- Clinica/SettingsVC.swift
+++ Clinica/SettingsVC.swift
@@ -9,11 +9,31 @@
 import UIKit
 import FirebaseFirestore
 import FirebaseAuth
-class SettingsVC: UIViewController {
 
-    @IBAction func deleteAccountTapped(_ sender: UIButton) {
-        print("Didn't work!!")
-       /* let db = Firestore.firestore()
+
+class SettingsVC: UIViewController {
+    
+    func signOut(){
+         let signinControl = storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.loginSignup) as? LoginSignUpPage
+        
+         view.window?.rootViewController = signinControl
+         view.window?.makeKeyAndVisible()
+     }
+    
+   
+    
+    @IBAction func ProfileTapped(_ sender: UIButton) {
+        
+        let profile = self.storyboard?.instantiateViewController(withIdentifier: "Profile")  as! ProfilePage
+        
+        self.navigationController?.pushViewController(profile, animated: true)
+    }
+    
+    
+    @IBAction func DeleteAccountTapped(_ sender: UIButton) {
+        
+        
+       let db = Firestore.firestore()
         
         let user = Auth.auth().currentUser
 
@@ -24,8 +44,20 @@ class SettingsVC: UIViewController {
           } else {
             // Account deleted.
               print("Account has been deleted successfully")
+              self.signOut()
           }
-        }*/
+        }
+    }
+    
+    @IBAction func SignOutTapped(_ sender: UIButton) {
+        
+        do {
+            try Auth.auth().signOut()
+           signOut()
+            
+        }catch let error {
+            print("failed to sign out with error...",error)
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Clinica/nsomnia.swift
@@ -0,0 +1,30 @@
+//
+//  nsomnia.swift
+//  Clinica
+//
+//  Created by htu on 24/09/2022.
+//  Copyright © 2022 HTU. All rights reserved.
+//
+
+import UIKit
+
+class nsomnia: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
